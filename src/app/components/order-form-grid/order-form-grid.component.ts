4703b51eddceb0511a0088b533f6c4f1965c0b0b
elwood-berry <eberry@anterasoftware.com>
2021-08-10 14:19:29 -0500

    5 files modified

--- src/app/components/order-form-grid/order-form-grid.component.ts
+++ src/app/components/order-form-grid/order-form-grid.component.ts
@@ -1,25 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 // INTERFACE: SIZES
-export interface StockItemSizes {
-  symbols: string[];
-}
+// export interface StockItemSizes: string[]
 
 // DATA: SIZES
-const ELEMENT_DATA_SIZES: StockItemSizes[] = [
-  {
-    symbols: ['XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL', '4XL', '5XL']
-  }
-];
+// const ELEMENT_DATA_SIZES: StockItemSizes[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL', '3XL', '4XL', '5XL'];
 
 @Component({
   selector: 'app-order-form-grid',
   templateUrl: './order-form-grid.component.html',
   styleUrls: ['./order-form-grid.component.scss']
 })
+//
 export class OrderFormGridComponent implements OnInit {
   // PROPERTIES
-  public symbols = ELEMENT_DATA_SIZES;
+  // public symbols = ELEMENT_DATA_SIZES;
 
   constructor() {}
 
