2bb0e4c671b0f40fc388ff897933e68b10e09444
elwood-berry <eberry@anterasoftware.com>
2021-08-11 09:37:32 -0500

    Updated

--- src/app/components/tabs-warehouses/tabs-warehouses.component.ts
+++ src/app/components/tabs-warehouses/tabs-warehouses.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs-warehouses',
   templateUrl: './tabs-warehouses.component.html',
-  styleUrls: ['./tabs-warehouses.component.css']
+  styleUrls: ['./tabs-warehouses.component.scss']
 })
 export class TabsWarehousesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
