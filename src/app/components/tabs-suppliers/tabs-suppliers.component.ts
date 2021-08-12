64e8f769446fb376872c65dc033d6a8d0fd7e8c9
elwood-berry <eberry@anterasoftware.com>
2021-08-12 08:23:27 -0500

    2 files modified, 1 added, 1 deleted

--- src/app/components/tabs-suppliers/tabs-suppliers.component.ts
+++ src/app/components/tabs-suppliers/tabs-suppliers.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs-suppliers',
   templateUrl: './tabs-suppliers.component.html',
-  styleUrls: ['./tabs-suppliers.component.css']
+  styleUrls: ['./tabs-suppliers.component.scss']
 })
 export class TabsSuppliersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
